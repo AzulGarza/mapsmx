de98010468dc0d95245e3620d5873eee4d31661a
Fede <fede.garza.ramirez@gmail.com>
2020-04-17 23:13:05 -0500

    wrong name

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="covidmx",
+    name="mapsmx",
     version="0.0.1",
     author="Federico Garza",
     author_email="fede.garza.ramirez@gmail.com",
-    description="Create maps of México easily with python..",
+    description="Create maps of México easily with python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/FedericoGarza/mapsmx",
