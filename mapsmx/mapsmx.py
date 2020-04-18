8efa384f72d3468b6d6033b5a3b72e837c0fc2b9
Fede <fede.garza.ramirez@gmail.com>
2020-04-17 22:47:44 -0500

    flake8 format

--- mapsmx/mapsmx.py
+++ mapsmx/mapsmx.py
@@ -1,36 +1,41 @@
 import geopandas as gpd
 
+
 class MapsMX:
     """
     Returns data to plot maps
     """
 
     def __init__(self):
-        pass
-
 
         self.dict_args_file = {'state': 'ent', 'municipality': 'mun'}
-
 
     def get_geo(self, kind, add_centroids=False):
 
         allowed_kinds = ('state', 'municipality')
 
-        assert kind in allowed_kinds, ValueError('kind must be one of {}'.format(', '.join(allowed_kinds)))
-
+        assert kind in allowed_kinds, \
+            ValueError(
+                'kind must be one of {}'.format(', '.join(allowed_kinds))
+            )
 
         kind_file = self.dict_args_file[kind]
 
-        read_file = 'zip://mapsmx/geo/{}.zip'.format(kind_file)
-        data = gpd.read_file(read_file)
-        data = self.clean_cols(data, kind_file)
-        data = data.set_geometry('geometry_ent')
-
-        if add_centroids:
-            data['centroid'] = data['geometry_{}'.format(kind_file)].centroid
+        data = self.read_data(kind_file, add_centroids)
 
         return data
 
+    def read_data(self, kind, add_centroids):
+
+        read_file = 'zip://mapsmx/geo/{}.zip'.format(kind)
+        data = gpd.read_file(read_file)
+        data = self.clean_cols(data, kind)
+        data = data.set_geometry('geometry_{}'.format(kind))
+
+        if add_centroids:
+            data['centroid'] = data['geometry_{}'.format(kind)].centroid
+
+        return data
 
     def clean_cols(self, df, kind):
 
@@ -39,7 +44,7 @@ class MapsMX:
                 'CVEGEO': 'cve_geo_{}'.format(kind),
                 'NOMGEO': 'NOM_{}'.format(kind),
                 'geometry': 'geometry_{}'.format(kind)
-        })
+            })
 
         df_clean.columns = df_clean.columns.str.lower()
 
